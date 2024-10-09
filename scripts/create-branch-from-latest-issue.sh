e4c94ee1752d9f622f68914ba031cc7d05b72849
Mark Beznos <mbeznos@kayak.com>
2024-10-09 09:30:40 -0400

    Added better uncommited changes selector

--- scripts/create-branch-from-latest-issue.sh
+++ scripts/create-branch-from-latest-issue.sh
@@ -13,8 +13,15 @@
 #  - i.e. support github CLI, gitlab CLI, linear.app?
 # - maybe rewrite this in like Python or GoLang
 # - write some test
+# - add a check that makes sure that ticket doesn't have a local branch already
+# - Add a JQL override
+# - Add options to stash/view/stash-and-restore changes if uncommited changes are on branch
+# - silence Git pull output
+# - add color output to some parts
+# - Paste ticket name and summary into clipboard (for ease of use)
 # Done:
 # - replaced FZF with jira-cli native selection
+# - Have dialog for uncommited changes
 
 set -f
 # split at newlines only
@@ -79,6 +86,33 @@ get_type_name() {
   fi
 }
 
+function _handle_changes_in_existing_branch() {
+  echo "Changes detected in current branch. What would you like to do?"
+  echo "1) Keep changes and continue"
+  echo "2) Stash changes and continue"
+  echo "3) View changes and repeat dialog"
+  echo "4) Exit script"
+  read -r -p "Please select an option: " choice
+  case $choice in
+    1)
+      ;;
+    2)
+      git stash
+      ;;
+    3)
+      git diff
+      _handle_changes_in_existing_branch
+      ;;
+    4)
+      exit 1
+      ;;
+    *)
+      echo "Invalid choice"
+      _handle_changes_in_existing_branch
+      ;;
+  esac
+}
+
 # Unsure why macOS has such a prehistoric version of bash
 # this is only needed because of the use of `read -i` not working on macOS
 if (($BASH_VERSINFO < 4)); then 
@@ -91,16 +125,16 @@ requires jira "https://github.com/ankitpokhrel/jira-cli/wiki/Installation"
 requires jq
 requires perl
 
+# default query - All open tickets either assigned to user, or created by user but unnasigned, sorted by latest
+QUERY='((assignee = currentUser()) OR (creator = currentUser() && assignee = EMPTY)) AND (statusCategory != "Done")' #formerly = In Progress
 
 if ! _GIT_STATUS=$(git status --porcelain); then
   echo "Not a git repository" >&2
   exit 1 
 fi
-# extract this as a function, so "show" can be called in a loop
+
 if [ -n "$_GIT_STATUS" ]; then
-  echo "There are uncommitted changes on the branch - Exiting" >&2
-  #options: show, stash, exit, continue
-  exit 1
+  _handle_changes_in_existing_branch
 fi
 
 LOCAL_TRUNK_NAME=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
@@ -111,7 +145,7 @@ me=$(jira me)
 # used if patch isn't installed
 #SELECTED_ISSUE=$(ccjira issue list -a$me --paginate 10 -sopen -s"In Progress" --plain | fzf --header-lines=1)
 # TODO: add custom JQL here? IO want to see all issues assigned to me, or created by me but unassigned
-SELECTED_ISSUE=$(JIRA_FORCE_INTERACTIVE=1 jira issue list -a"$me" -sopen -s"In Progress" --json --select-on-enter)
+SELECTED_ISSUE=$(JIRA_FORCE_INTERACTIVE=1 jira issue list -q "$QUERY" --order-by created --json --select-on-enter)
 if [ -z "$SELECTED_ISSUE" ]; then
   echo "No issue selected" >&2
   exit 1
@@ -137,5 +171,5 @@ if [ "$LOCAL_TRUNK_NAME" != "$_CURRENT_BRANCH" ]; then
   git switch $LOCAL_TRUNK_NAME
 fi
 
-git pull
+git -c gc.auto=0 pull
 git switch -c $chosen_branch_name
