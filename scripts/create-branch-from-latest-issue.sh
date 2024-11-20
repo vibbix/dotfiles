0de99f43bd842132196053cfe08ee6a0f021a5bc
Mark Beznos <mbeznos@kayak.com>
2024-11-20 09:38:42 -0500

    - updated create branch scripts  - added changelog

--- scripts/create-branch-from-latest-issue.sh
+++ scripts/create-branch-from-latest-issue.sh
@@ -19,21 +19,33 @@
 # - silence Git pull output
 # - add color output to some parts
 # - Paste ticket name and summary into clipboard (for ease of use)
+# - handle remote branches in _does_branch_exist
 # Done:
 # - replaced FZF with jira-cli native selection
 # - Have dialog for uncommited changes
 # - Add colors
 # - Added colon to santize branch name
 
+# CHANGELOG
+# [V0.1.8] - 11/20/2024
+# - Updated query to sort tickets by status
+# [v0.1.7] - 11/02/2024
+# - Added Additonal colors to map
+# - localized some functions
+# - Improved handle changes in current branch 
+# - changes check now occurs after ticket selection
+# - Added check for existing branches
+
 set -f
 # split at newlines only
 IFS='
 '
 
-RED="\e[31m"
-GREEN="\e[32m"
-NC="\e[0m"
-
+RED=$(tput setaf 1)
+GREEN="$(tput setaf 2)"
+BLUE=$(tput setaf 4)
+NC="$(tput sgr0)"
+VERSION="v0.1.8"
 
 # IFS=$'\n'
 # requires see's if the utility is installed
@@ -59,7 +71,7 @@ kebab_case() {
   perl -ne 'print lc s/[[:lower:]\d]\K[[:upper:]]/-$&/rg' <<< $(sed 's/ /-/g' <<< $1)
 }
 
-trim()
+_trim()
 {
     local trimmed="$1"
     # Strip leading spaces.
@@ -75,31 +87,35 @@ trim()
 
 sanitize_branch_name() 
 {
-  trim "$1" | \
+  _trim "$1" | \
     tr -d '\r' | \
     tr -d '\n' | \
     tr -d '\t' | \
-    sed -E -e 's/(^|[\/.])([\/.]|$)|:^@$|@{|[\x00-\x20\x7f~^:?*;"'"'"'`&|$()!\][\\]|\.lock(\/|$)|\S/-/g' -e 's/-{2,}/-/g'
+    sed -E -e 's/(^|[\/.])([\/.]|$)|:|#|\||^@$|@{|[\x00-\x20\x7f~^?*;"'"'"'`&|$()!\][\\]|\.lock(\/|$)|\S/-/g' -e 's/-{2,}/-/g'
 }
 
+# Convert the Task-type to a good prefix for the branch name
+# TODO: have this be disaable with a param
 get_type_name() {
-  if [ $1 == "Task" ]; then
+  local ticket_type="$1"
+  if [ $ticket_type == "Task" ]; then
     echo "feature"
-  elif [ $1 == "Sub-Task" ]; then
+  elif [ $ticket_type == "Sub-Task" ]; then
     echo "feature"
-  elif [ $1 == "Bug" ]; then
+  elif [ $ticket_type == "Bug" ]; then
     echo "bugfix"
   else
-    echo "$1" | tr "[:upper:]" "[:lower:]"
+    echo "$ticket_type" | tr "[:upper:]" "[:lower:]"
   fi
 }
 
 function _handle_changes_in_existing_branch() {
-  echo "${RED}$1${NC} Changes detected in current branch. What would you like to do?"
-  echo "${RED}1)$NC Keep changes and continue"
-  echo "${RED}2)$NC Stash changes and continue"
-  echo "${RED}3)$NC View changes and repeat dialog"
-  echo "${RED}4)$NC Exit script"
+  local changes="$1"
+  printf "${RED}%d${NC} Changes detected in current branch. What would you like to do?\n" "$changes"
+  printf "${RED}1)$NC Do nothing and continue\n"
+  printf "${RED}2)$NC Stash changes and continue\n"
+  printf "${RED}3)$NC View changes and repeat dialog\n"
+  printf "${RED}4)$NC Exit script\n"
   read -r -p "Please select an option: " choice
   case $choice in
     1)
@@ -110,16 +126,48 @@ function _handle_changes_in_existing_branch() {
       ;;
     3)
       git diff
-      _handle_changes_in_existing_branch "$1"
+      _handle_changes_in_existing_branch "$changes"
       ;;
     4)
       exit 1
       ;;
     *)
       echo "Invalid choice"
-      _handle_changes_in_existing_branch "$1"
+      _handle_changes_in_existing_branch "$changes"
       ;;
   esac
+}
+
+function _does_branch_exist() {
+  local ticket_id="$1"
+  # TODO: verify that no preceding charcater breaks this (i.e. ENG vs RENG)
+  local matched_tickets
+  #TODO - handle remote branches
+  if matched_tickets="$(git branch --list --remote | grep -E ".*${ticket_id}[^0-9]+.*")"; then
+    #echo "Branch already exists for ticket: ${RED}${ticket_id}${NC}"
+    local _count="$(wc -l <<< "$matched_tickets")"
+    if [ $_count -lt 2 ]; then 
+      printf "Found branch: ${RED}%s${NC}\n" "$matched_tickets"
+      read -r -p "Would you like to swap?(${GREEN}Y{$NC}/${RED}N${NC}): " choice
+      case $choice in
+        y) 
+          git switch "$matched_tickets"
+          printf "Switched to branch: ${GREEN}%s${NC}\n" "$matched_tickets"
+          exit 1
+          ;;
+        n)
+          ;;
+        *)
+          printf "Invalid choice - ${RED}quitting${NC}\n"
+          exit 1
+          ;;
+      esac
+    else
+      echo "$_count branches found for ticket: ${ticket_id}"
+      echo "quitting"
+      exit 1
+    fi
+  fi
 }
 
 # Unsure why macOS has such a prehistoric version of bash
@@ -135,16 +183,11 @@ requires jq
 requires perl
 
 # default query - All open tickets either assigned to user, or created by user but unnasigned, sorted by latest
-QUERY='((assignee = currentUser()) OR (creator = currentUser() && assignee = EMPTY)) AND (statusCategory != "Done")' #formerly = In Progress
+QUERY='((assignee = currentUser()) OR (creator = currentUser() && assignee = EMPTY)) AND (statusCategory != "Done") ORDER BY status DESC' #formerly = In Progress
 
 if ! _GIT_STATUS=$(git status --porcelain); then
   echo "Not a git repository" >&2
   exit 1 
-fi
-
-if [ -n "$_GIT_STATUS" ]; then
-  _changes=$(trim "$(wc -l <<< $_GIT_STATUS)")
-  _handle_changes_in_existing_branch "$_changes"
 fi
 
 LOCAL_TRUNK_NAME=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
@@ -159,26 +202,38 @@ if [ -z "$SELECTED_ISSUE" ]; then
   echo "No issue selected" >&2
   exit 1
 fi
+#check for changes
+if [ -n "$_GIT_STATUS" ]; then
+  _changes=$(_trim "$(wc -l <<< $_GIT_STATUS)")
+  if [ $_changes -gt 0 ]; then
+    _handle_changes_in_existing_branch "$_changes"
+  fi
+fi
 
 #TODO: assert array is one
 for s in $(echo "$SELECTED_ISSUE" | jq -r ".[0]|to_entries|map(\"\(.key)=\(.value|tostring)\").[]"); do
   # TODO: o i need export here?
   export _$s
 done
+_does_branch_exist "$_key"
+
 default_branch_name=$(sanitize_branch_name $(printf "%s/%s-%s" $(get_type_name $_type) $_key $(kebab_case $_summary)))
 
 echo "What would you like your branch name to be? (default: '$default_branch_name'):"
 read -r -e -i "$default_branch_name" chosen_branch_name
 
 #TODO: santize branch name
-echo "Creating branch: ${GREEN}$chosen_branch_name${NC}"
+printf "Creating branch: ${GREEN}%s${NC}\n" "$chosen_branch_name"
 
 _CURRENT_BRANCH=$(git branch --show-current)
 
 if [ "$LOCAL_TRUNK_NAME" != "$_CURRENT_BRANCH" ]; then
-  echo "Currently on ${RED}${_CURRENT_BRANCH}${NC}. Switching to ${GREEN}${LOCAL_TRUNK_NAME}${NC}"
+  printf "Currently on ${RED}%s${NC}. Switching to ${GREEN}%s${NC}\n" "$_CURRENT_BRANCH" "$LOCAL_TRUNK_NAME"
   git switch "$LOCAL_TRUNK_NAME"
 fi
 
+#Prevent auto GC form running
+#TODO: optional check for this
+#TODO: have git GC write to stderr, or just dropped entirely
 git -c gc.auto=0 pull
 git switch -c "$chosen_branch_name"
